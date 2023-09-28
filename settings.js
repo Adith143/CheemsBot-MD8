1ccad49be1e2ec811333886dffbb0d3c5ef11a57
Adith 143 <110350756+Adith143@users.noreply.github.com>
2023-09-28 12:30:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['919747977735'] //ur owner number
+global.ownernomer = "919747977735" //ur owner number2
+global.ownername = "🦄Lord Adith" //ur owner name
+global.ytname = "YT: Adith 143" //ur yt chanel name
+global.socialm = "GitHub: Adith143" //ur github or insta name
+global.location = "India, kerala, Thiruvananthapuram" //ur location
 
 //new
 global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.ownernumber = '919747977735'
+global.ownername = '🦄드림 가이 Adith'
+global.ownerNumber = ["919747977735@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@Adith_143"
+global.websitex = "https://youtu.be/0wEvlfX1Ad8?si=kUtf74FEGmQPR6v7"
+global.wagc = "https://chat.whatsapp.com/DdBbUKsl9OL75b9hmYIuPM"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "Sukuna Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 Sukunq\n\n"
+global.creator = "919747977735@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
